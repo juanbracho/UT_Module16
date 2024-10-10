e66388f8ac62bf64bea4b58249996cb861c4ed0b
Juan Bracho <64031972+juanbracho@users.noreply.github.com>
2024-10-10 17:03:32 -0500

    .

--- app.py
+++ app.py
@@ -13,15 +13,5 @@ df_tele = pd.read_csv('data/filtered_tele_data.csv')
 def index():
     return render_template('index.html')
 
-# Route to serve oil data as JSON (optional)
-@app.route('/oil_data')
-def oil_data():
-    return df_oil.to_json(orient='records')
-
-# Route to serve telecom data as JSON (optional)
-@app.route('/tele_data')
-def tele_data():
-    return df_tele.to_json(orient='records')
-
 if __name__ == '__main__':
     app.run(debug=True)
